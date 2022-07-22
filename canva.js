f67f724255ffd0a7a7c7815b120940b8460138d0
brinkz <isabel.sbg11@gmail.com>
2022-07-21 22:46:22 -0600

    reto 1: crear opuesto

--- canva.js
+++ canva.js
@@ -2,8 +2,16 @@ var d = document.getElementById("cuadrito_canva");
 var lienzo = d.getContext("2d");
 var lineas = 30;
 var l = 0;
+var xi, yf;
 var yi, xf;
 var colorcito = "red";
+
+for(l =0; l < 30; l++){
+    xi = 300 - 10*(l+1);
+    yf = 300 - 10 * l;
+    dibujarLinea(colorcito, xi, 0, 300, yf);
+    console.log("Linea " + l);
+}
 
 for(l =0; l < 30; l++){
     yi = 10 * l;
@@ -14,6 +22,8 @@ for(l =0; l < 30; l++){
 
 dibujarLinea(colorcito, 1, 1, 1, 300);
 dibujarLinea(colorcito, 1, 299, 299, 299);
+dibujarLinea(colorcito, 299, 0, 299, 299);
+dibujarLinea(colorcito, 299, 1, 1, 1);
 
 function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal){
     lienzo.beginPath();
